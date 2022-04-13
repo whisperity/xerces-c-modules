e0b11284c56ba8d061ae83145969c14295123f42
Whisperity <whisperity@gmail.com>
2022-04-13 13:51:44 +0200

    fix: Remove dummy empty file

--- Modules.cmake
+++
@@ -1,3 +0,0 @@
-message(FATAL_ERROR "Placeholder script found for Modules.cmake script of "
-                    "ModulesTSMaker. Please run the tool first to deploy the "
-                    "real script into this location.")
